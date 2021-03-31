9cbe8aebc3af8e5e81525ca2d7ea2f0f708747c7
stevendrawings <stevenchauvets@gmail.com>
2021-03-31 16:38:38 +0200

    version 0

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Tuto php</title>
+</head>
+<body>
+    <?php  
+
+    // Les variables
+        // $nom = "David";
+        // $note = 10;
+        // $note2 = 12;
+        // $resultat = ($note + $note2) / 2;
+        // echo "Je m'appelle ". $nom ." et ma moyenne". "\n". "est de". $resultat. " sur 20 ";
+
+    // Les tableaux
+        // $notes = [10, 20, 7, 43, 4];
+        // echo $notes[2]
+
+        // $eleve = ["Marc", "Doe", [10, 20, 50] ];
+        // echo $eleve[2][1]
+
+        // $eleve = [ 
+        //     "nom" => "David", 
+        //     "prenom" => "Kevin", 
+        //     "notes" => [10, 40, 32, 50]
+        // ];
+        // $eleve["prenom"] = "Jean";
+        // $eleve["notes"][3] = 12;
+        // echo $eleve["nom"] . " " . $eleve["prenom"];
+        // print_r($eleve["notes"])
+
+        // $classe = [
+        //     [
+        //         "nom" => "Doe",
+        //         "prenom" => "John",
+        //         "notes" => [10, 15, 15]
+        //     ],
+        //     [
+        //         "nom" => "Doe",
+        //         "prenom" => "John",
+        //         "notes" => [10, 12, 11]
+        //     ]
+        //     ];
+        // echo $classe[0]["notes"][1]
+
+    // Les conditions
+
+        // $notes = 10;
+        // if($notes > 10){
+        //     echo "Bravo vous avez la moyennne";
+        // } elseif($notes == 10) {
+        //     echo "Vous avez juste la moyenne";
+        // } else {
+        //     echo "Dommage vous n'avez pas la moyenne";
+        // }
+
+        // $heure = 14;
+
+        // if((9 <= $heure && $heure <= 12) || (14 <= $heure && $heure <= 17)){
+        //     echo "le salon est ouvert";
+        // } else {
+        //     echo "le salon n'est pas ouvert";
+        // }
+    // Les bloucles
+
+        // $reponse = null;
+
+        // while($reponse !== 10){
+
+        //     echo "vous avez trouvez la reponse !";
+
+        // } 
+
+        // $vehicules = [
+            
+        //     'voiture_italienne' => [
+        //         "ferrari", 
+        //         "Lamborghuini", 
+        //         "matzerati"
+        //     ],
+
+        //     'voiture_francaise' => [
+        //         "renault", 
+        //         "peugeot", 
+        //         "Dacli"
+        //     ]
+        // ];
+
+        // foreach($vehicules as $categoryze => $liste_vehicule){
+        //     echo "Voici les $categoryze";
+        //     foreach($liste_vehicule as $vehicule) {
+        //         echo "$vehicule";
+        //     }
+        // }
+
+
+        // for($i = 0; $i < 3; $i = $i + 1){
+        //     echo "-" . $note[$i];
+        // }
+
+    
+
+
+
+    ?>
+</body>
+</html>
